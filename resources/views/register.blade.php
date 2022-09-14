8fea51b9ad594d0d16fd9b40ffd119c65af3e6ba
FavianHilmi <favianhilmi11@gmail.com>
2022-09-14 13:47:57 +0700

    menambahkan view register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Momma | Login</title>
-    <link href="css/styleslog.css" rel="stylesheet" />
+    <link href="css/stylesreg.css" rel="stylesheet" />
 </head>
 <body>
 <div class="wrapper">
@@ -21,13 +21,21 @@
                 <input type="text" name="userName" id="userName" placeholder="Username">
             </div>
             <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="email" id="email" placeholder="Email">
+            </div>
+            <div class="form-field d-flex align-items-center">
                 <span class="fas fa-key"></span>
                 <input type="password" name="password" id="pwd" placeholder="Password">
             </div>
-            <button class="btn mt-3">Login</button>
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="password" id="pwd" placeholder="Confirm Password">
+            </div>
+            <button class="btn mt-3">Register</button>
         </form>
         <div class="text-center fs-6">
-            <a href="#">Forget password?</a> or <a href="/register">Sign up</a>
+            Already have an account? <a href="/login">Login now</a>
         </div>
     </div>
 </body>
